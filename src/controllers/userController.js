af67b53236b8b7d785b01b6734e9501f9d160cbe
Lilian Mathu <lilianmathu2018@gmail.com>
2022-10-25 01:24:22 +0300

    Add get / route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,6 +25,19 @@ const userController = {
       res.status(500).json({ message: "Failed to save user!", error });
     }
   },
+
+  listUser: async (req, res) => {
+    try {
+      const users = await User.find({});
+
+      res.status(200).json({
+        count: users.length,
+        users,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Failed to get /", error });
+    }
+  },
 };
 
 export default userController;
